e46f352244bf4d6345dd035f265849e380aafb25
Jesús Alberto Martínez Mendoza <hongo-loco@hotmail.com>
2018-02-02 00:14:54 -0600

    Interfaz por @lilia-casth

--- Superposiciones/Form1.Designer.cs
+++ Superposiciones/Form1.Designer.cs
@@ -28,12 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.botCarga = new System.Windows.Forms.Button();
+            this.textCarga = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.segmento = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dirRelativa = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dirReal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.longitud = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(667, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            this.ayudaToolStripMenuItem.Click += new System.EventHandler(this.ayudaToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(28, 85);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(169, 192);
+            this.treeView1.TabIndex = 1;
+            // 
+            // botCarga
+            // 
+            this.botCarga.Location = new System.Drawing.Point(236, 51);
+            this.botCarga.Name = "botCarga";
+            this.botCarga.Size = new System.Drawing.Size(75, 23);
+            this.botCarga.TabIndex = 2;
+            this.botCarga.Text = "Cargar";
+            this.botCarga.UseVisualStyleBackColor = true;
+            this.botCarga.Click += new System.EventHandler(this.botCarga_Click);
+            // 
+            // textCarga
+            // 
+            this.textCarga.Location = new System.Drawing.Point(352, 54);
+            this.textCarga.Name = "textCarga";
+            this.textCarga.Size = new System.Drawing.Size(77, 20);
+            this.textCarga.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.segmento,
+            this.dirRelativa,
+            this.dirReal,
+            this.longitud});
+            this.listView1.Location = new System.Drawing.Point(236, 85);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(244, 192);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // segmento
+            // 
+            this.segmento.Text = "Segmento";
+            // 
+            // dirRelativa
+            // 
+            this.dirRelativa.Text = "Dir. Relativa";
+            // 
+            // dirReal
+            // 
+            this.dirReal.Text = "Dir. Real";
+            // 
+            // longitud
+            // 
+            this.longitud.Text = "Longitud";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ingrese la dirección de carga";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Árbol de Trayectorias";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 402);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textCarga);
+            this.Controls.Add(this.botCarga);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button botCarga;
+        private System.Windows.Forms.TextBox textCarga;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader segmento;
+        private System.Windows.Forms.ColumnHeader dirRelativa;
+        private System.Windows.Forms.ColumnHeader dirReal;
+        private System.Windows.Forms.ColumnHeader longitud;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Superposiciones/Form1.cs
+++ Superposiciones/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,62 @@ namespace Superposiciones
 {
     public partial class Form1 : Form
     {
+        public OpenFileDialog opFile;
+        public string confText, longText, dirCargaStr;
+        public int dirCarga;
+          
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        //carga de configuración
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            confText = readFile();
+        }
+        //carga de longitudes
+        private void botCarga_Click(object sender, EventArgs e)
+        {
+            longText = readFile();
+
+            //recuperar la dirección de carga
+            dirCargaStr = textCarga.Text;
+            //convertir a int la cadena en hex
+            dirCarga = Int32.Parse(dirCargaStr, System.Globalization.NumberStyles.HexNumber);
+            
+        }
+
+        private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AyudaForm pantAyuda = new AyudaForm();
+            pantAyuda.Show();
+        }
+
+        //exit
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //método que lee archivo y abre ventana para elegirlo, 
+        //regresa una cadena con todo el texto contenido
+        private string readFile()
+        {
+            string fileText;
+            opFile = new OpenFileDialog();
+            opFile.Filter = "Configuration Files|*.txt";
+            opFile.Title = "Choose a configuration file .txt";
+
+            if (opFile.ShowDialog() != DialogResult.OK)
+                return null;
+            fileText = File.ReadAllText(opFile.FileName);
+            return fileText;
         }
     }
 }
